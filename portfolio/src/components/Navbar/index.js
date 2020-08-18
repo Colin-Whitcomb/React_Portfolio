644067402ad9a38d98f4ae71280913b6963d9a2e
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-17 17:28:41 -0700

    [add] component file structure. [add] Navbar component.

---
+++ portfolio/src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-dark bg-dark float right">
+            <a className="navbar-brand" href="/">Projects</a>
+            <a className="navbar-brand" href="/">Assignments</a> 
+            <a className="navbar-brand" href="/">Contact</a> 
+        </nav>
+    )
+} 
+
+export default Navbar;
