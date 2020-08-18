4d413c5f77e0f3d5cd222428a0c35175c6ee815c
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-17 17:47:55 -0700

    [add] jumbotron.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
 
 
 function App() {
   return (
    <div>
      <Navbar/>
+     <Jumbotron/>
      <h1>Hello!</h1>
    </div>
    
---
+++ portfolio/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4 text-center">Colin Whitcomb</h1>
+                <p className="lead text-center">Self motivated web developer eager to work together to create something amazing.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
