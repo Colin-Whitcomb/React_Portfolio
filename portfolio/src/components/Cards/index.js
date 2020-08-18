6b47fb4153d91831c278364dfdfe3feb1f53ecff
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-17 19:21:28 -0700

    [edit] responsive layout; project cards.

--- portfolio/src/components/Cards/index.js
+++ portfolio/src/components/Cards/index.js
@@ -11,6 +11,7 @@ function Cards() {
     <div className="container">
 
         <div className="row">
+
             <div class="col-md-6 sm-12">
                 <div class="card bg-dark text-white">
                     <img src={wip} class="card-img" alt="..."/>
@@ -21,44 +22,48 @@ function Cards() {
             </div>
 
                         <div class="col-md-6 sm-12">
-                         
-                            <h4 class="card-text align-text-bottom text-center">WiP is a goal oriented progress tracker app that helps the user divide and conquer goals.</h4>
-                            <br/>   
+
                             <div className='row'>
-                                <div class="col-md-6">
+                            <h4 class="card-text align-text-bottom text-center">WiP is a goal oriented progress tracker app that helps the user divide and conquer goals.</h4>
+                                 
+                               
+                            
+                                <div class="col-xs-6 justify-content-center">
                                     <img src='https://media.giphy.com/media/IecI9ZHXpzb8kMXKQg/giphy.gif' alt='gif' class='gif'/>
                                 </div>
                                 <br/>  <br/>  <br/>  
-                                <div class="col-md-6">
+                                <div class="col-xs-6">
                                     <button className="btn btn-dark">Deployed Link</button>
                                         <br/> <br/>
                                     <button className="btn btn-dark">GitHub</button>
                                 </div>
                             </div>
                         </div>
-                </div>
+        </div>
          
-<br/><br/><br/><br/>
+        <br/><br/><br/><br/>
+   
         <div className="row">
-            <div class="col-md-6">
+            <div class="col-md-6 sm-12">
                 <div class="card bg-dark text-white">
                     <img src={miles} class="card-img" alt="..."/>
                         <div class="card-img-overlay">
-                            <h3 class="card-title text-center">WiP</h3>
+                            <h3 class="card-title text-center">Miles</h3>
                         </div>
                 </div>
             </div>
 
-            <div class="col-sm-6">
-                         
+            <div class="col-md-6 sm-12">
+                        
+            <div className='row'>
             <h4 class="card-text text-center">Miles is an app designed to help find close and healthy eating destinations.</h4>
                          <br/>   
-                         <div className='row'>
-                             <div class="col-sm-6">
+                       
+                             <div class="col-xs-6">
                                  <img src={MilesGif} alt='gif' class='gif'/>
                              </div>
 
-                             <div class="col-sm-6">
+                             <div class="col-xs-6">
                                  <button className="btn btn-danger">Deployed Link</button>
                                      <br/> <br/>
                                  <button className="btn btn-danger">GitHub</button>
