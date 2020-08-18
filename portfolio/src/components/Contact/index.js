581e04be908559a474cc58e25b994a0be8593f82
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-18 09:48:20 -0700

    [add] Resume | LinkedIn.

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -3,18 +3,24 @@ import React from 'react';
 function Contact() {
     return (
         <div className="container border mt-4">
-            <h4 className='text-center'>Want to get in touch?</h4>
+            <h4 className='text-center'>Want to connect?</h4>
                     
             <div className="row">
-                <div className='col'>
-                    <th className="text-center">Contact Info</th>
+                <div className='col offset-2'>
+                    <th>Contact Info</th>
                         <tr>Email: csammyw@gmail.com</tr>
                         <tr>Phone: (864) 612-0622</tr>
                     </div>
                 <div className='col'>
-                    <th>Contact Info</th>
-                        <tr>Email: csammyw@gmail.com</tr>
-                        <tr>Phone: (864) 612-0622</tr>
+                    <th>Experience</th>
+                        <a href="https://www.linkedin.com/in/colin-whitcomb/" target="_blank">
+                            <tr>LinkedIn</tr>
+                        </a>
+                        <br/>
+                        <a href="https://docs.google.com/document/d/1ft7R_n3xi_CKFITbdV4TJbNVcQ4nb2SSQ1qEhh4Kqig/edit?usp=sharing" target="_blank">
+                            <tr>Resume</tr>
+                        </a>
+                        
                 </div>
             </div>
         </div>
