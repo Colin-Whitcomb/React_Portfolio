d36df56610e3cbffc81c1cfef5859f600ed7bba2
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-18 05:10:15 -0700

    [update] Sushi Lover Card.

--- portfolio/src/components/hwCards/index.js
+++ portfolio/src/components/hwCards/index.js
@@ -5,16 +5,16 @@ import './style.css';
 function HomeworkCards() {
     return(
         <div className="container">
-            <div class="card">
+            <div class="card mt-2 hw">
                 <img class="card-img-top" src="https://media.giphy.com/media/SU8qHfqp7zL67I1nnW/giphy.gif" alt="Card image cap"/>
                     <div class="card-body">
-                        <h5 class="card-title">Sushi Lover</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text"><b>Sushi Lover</b> is an app that allows the user to add and "devour" it their
+                        favorite sushi order!</p>
 
                         <div className='row'>
-                        <a href="#" class="btn btn-primary">Deployed Site</a>
+                        <a href="https://nameless-bastion-18794.herokuapp.com/" target="_blank" class="btn btn-primary">Deployed Site</a>
                       
-                        <a href="#" class="btn btn-primary ml-1">GitHub</a>
+                        <a href="https://github.com/Colin-Whitcomb/sushi_lover" target="_blank" class="btn btn-primary ml-1">GitHub</a>
                         </div>
                       
                     </div>
