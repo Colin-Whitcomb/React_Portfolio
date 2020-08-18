064df9df38825f44de21b8a7d54246f0b24c52bb
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-18 08:55:27 -0700

    [add] LinkedIn logo.

--- portfolio/src/components/Jumbotron/index.js
+++ portfolio/src/components/Jumbotron/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import linkedin from '../../images/linkedin-logo.png';
+import "./style.css";
 
 function Jumbotron() {
     return (
@@ -10,7 +12,10 @@ function Jumbotron() {
                     <div className="col-sm-12 offset-2">
                         <h1 className="display-4 d-inline">Colin Whitcomb</h1> 
                         <a className="btn btn-dark mr-2 ml-2 mb-4" href="https://www.linkedin.com/in/colin-whitcomb/" target="_blank">LinkedIn</a>
-                        <a className="btn btn-dark mb-4" href="https://github.com/Colin-Whitcomb" target="_blank">GitHub</a>
+
+                        <a href="https://www.linkedin.com/in/colin-whitcomb/" target="_blank">
+                            <img className="btn mb-4 linkedIn" target="_blank" src="https://image.flaticon.com/icons/png/512/61/61109.png"></img>
+                        </a>
                     </div>  
                     
                     <div className="col-sm-12">
