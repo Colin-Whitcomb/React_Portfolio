56d36e7d4eaf52afe0b6e5b2221cd42a29960684
Colin Whitcomb <colinsw@berkeley.edu>
2020-09-10 16:31:51 -0700

    [updated] AM styling.

--- portfolio/src/components/AboutMe/index.js
+++ portfolio/src/components/AboutMe/index.js
@@ -6,17 +6,25 @@ function AboutMe() {
     return (
         <div className="aM">
             <div className="container mt-4"  id="aboutMe">
-                <div class="media">
-                    <img class="mr-3 mt-2 headShot rounded" src={headshot} alt="headShot"/>
+                <div className='row'>
+
+                    <div className='col-lg-4 col-md-6'>
+                        <div className='text-center'>
+                            <img class="mr-3 mt-2 headShot rounded" src={headshot} alt="headShot"/>
+                        </div>
+                    </div>
+
+                    <div className='col-lg-8 col-md-6'>
                         <div class="media-body text-wrap">
                             <h3 class="mt-2">About Me</h3>
                                 <hr/>
-                            <h5>Full Stack Web Developer with previous experience in Music Education. Effective communicator and passionate about optimizing team strategies. Self-motivated and goal-oriented to ensure work upholds high standards.
+                            <h6>Full Stack Web Developer with previous experience in Music Education. Effective communicator and passionate about optimizing team strategies. Self-motivated and goal-oriented to ensure work upholds high standards.
                                     <br/><br/>
-                                    For the past 8 years, I've been teaching percussion to a variety of students from middle school through college in both southern and northern California. Recently, however, I have pivoted my career into Full Stack Web Development by enrolling in the UCB Extension's Coding Bootcamp! 
-                            </h5>
-
+                                For the past 8 years, I've been teaching percussion to a variety of students from middle school through college in both southern and northern California. Recently, however, I have pivoted my career into Full Stack Web Development by enrolling in the UCB Extension's Coding Bootcamp! 
+                            </h6>
                         </div>
+                    </div>
+                        
                 </div>
             </div>
         </div>
