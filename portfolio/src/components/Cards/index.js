952bc5297989ff31a2f57c11c780ff06dfbc63ae
Colin Whitcomb <colinsw@berkeley.edu>
2020-09-10 19:10:24 -0700

    [updated] Project Cards.

--- portfolio/src/components/Cards/index.js
+++ portfolio/src/components/Cards/index.js
@@ -10,87 +10,48 @@ function Cards() {
     
     <div className="container mt-2" id="projects">
 
-<br/>   
-        <h2 className="text-center mt-6" id="teamHeading">Team Projects</h2>
+        <br/>   
+            <h2 className="mt-1" id="teamHeading">Team Projects</h2>
+            <hr></hr>
+    {/* ------------- WiP ---------- */}
+            <div className="row">
 
-        <br/><br/>
-
-        <div className="row mt-1">
-
-            <div class="col-md-6 sm-12">
-                <div class="card bg-dark text-white">
-                    <img src={wip} class="card-img" alt="..."/>
-                        <div class="card-img-overlay">
-                            <h3 class="card-title text-center">WiP</h3>
+                <div class="col-md-6 sm-12">
+                    <p><span class='extraWords'><b>WIP</b></span> is a goal oriented progress tracker app that helps the user divide and conquer goals.</p>
+                        <div class="card bg-dark text-white">
+                            <img src={wip} className="card-img center-text" id='wipPic' alt="..."/>
                         </div>
+
+                        <br></br>
+
+                        <a href="https://vast-hollows-48558.herokuapp.com" target="_blank" className='text-center'>
+                            <button className="btn btn-dark">Deployed Link</button>
+                        </a>
+                        <a href="https://github.com/crackedsnowboard/project-maven" target="_blank" className='text-center ml-1'>
+                            <button className="btn btn-dark">GitHub</button>
+                        </a>
                 </div>
             </div>
-
-                        <div class="col-md-6 sm-12">
-
-                            <div className='row'>
-                            <h4 class="card-text align-text-bottom ml-3">WiP is a goal oriented progress tracker app that helps the user divide and conquer goals.</h4>
-                                 
-                            
-                            
-                                <div class="col-xs-6 mt-3">
-                                    <img src='https://media.giphy.com/media/IecI9ZHXpzb8kMXKQg/giphy.gif' alt='gif' class='img'/>
-                                </div>
-                                <br/>  <br/>  <br/>  
-                                <div class="col-xs-5 offset-1 mt-3">
-                                    <a href="https://vast-hollows-48558.herokuapp.com" target="_blank">
-                                        <button className="btn btn-dark">Deployed Link</button>
-                                    </a>
-
-                                        <br/> <br/>
-
-                                    <a href="https://github.com/crackedsnowboard/project-maven" target="_blank">
-                                        <button className="btn btn-dark">GitHub</button>
-                                    </a>
-
-                                </div>
+            <br></br>
+    {/* ------------- MILES ---------- */}
+            <div className="row">
+                <div class="col-md-6 sm-12">
+                        <p class="card-text"><span class='extraWords'><b>Miles</b></span> is an app designed to help find close and healthy eating destinations.</p>
+                            <div class="card bg-dark text-white">
+                                <img src={miles} class="card-img" id='milesPic' alt="..."/>
                             </div>
-                        </div>
-        </div>
-         
-        <br/><br/><br/><br/>
-   
-        <div className="row">
-            <div class="col-md-6 sm-12">
-                <div class="card bg-dark text-white">
-                    <img src={miles} class="card-img" alt="..."/>
-                        <div class="card-img-overlay">
-                            <h3 class="card-title text-center">Miles</h3>
-                        </div>
+                                <br></br>
+                        <a href="https://gabesucich.github.io/Project1_CGJK/" target="_blank">
+                            <button className="btn btn-dark">Deployed Link</button>
+                        </a>
+                        <a href="https://github.com/Colin-Whitcomb/Project1_CGJK" target="_blank">
+                            <button className="btn btn-dark ml-1">GitHub</button>
+                        </a>
                 </div>
+
+            
             </div>
-
-            <div class="col-sm-6">
-                        
-            <div className='row'>
-            <h4 class="card-text text-center">Miles is an app designed to help find close and healthy eating destinations.</h4>
-                         <br/>   
-                       
-                             <div class="col-xs-6 mt-3">
-                                 <img src={MilesGif} alt='gif' class='img'/>
-                             </div>
-
-                             <div class="col-xs-5 offset-1 mt-3">
-
-                                     <a href="https://gabesucich.github.io/Project1_CGJK/" target="_blank">
-                                        <button className="btn btn-dark">Deployed Link</button>
-                                    </a>
-
-                                        <br/> <br/>
-
-                                    <a href="https://github.com/Colin-Whitcomb/Project1_CGJK" target="_blank">
-                                        <button className="btn btn-dark">GitHub</button>
-                                    </a>
-                             </div>
-                         </div>
-                     </div>
-             </div>
-             <br/>
+        <br/>
     </div>
     
     )
