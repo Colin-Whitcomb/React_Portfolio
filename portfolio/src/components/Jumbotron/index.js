257536ca05cdd6efa819894875ef7d0425060dd1
Colin Whitcomb <colinsw@berkeley.edu>
2020-09-11 19:31:16 -0700

    [updated] jumbo

--- portfolio/src/components/Jumbotron/index.js
+++ portfolio/src/components/Jumbotron/index.js
@@ -13,14 +13,14 @@ function Jumbotron() {
                     <div className="col-sm-12 text-center mt-3">
                         <h3 className="mt-2 display-4 d-inline">COLIN WHITCOMB</h3> 
 
-                        <a className="btn mb-4" href="https://github.com/Colin-Whitcomb/" target="_blank">
+                        {/* <a className="btn mb-4" href="https://github.com/Colin-Whitcomb/" target="_blank">
                             <img className='btn linkImg' src={gitHubLogo}/>
                         </a>
 
 
                         <a href="https://www.linkedin.com/in/colin-whitcomb/" target="_blank">
                             <img className="btn mb-4 linkImg" target="_blank" src="https://image.flaticon.com/icons/png/512/61/61109.png"></img>
-                        </a>
+                        </a> */}
                     </div>  
                     
                     <div className="col-sm-12">
